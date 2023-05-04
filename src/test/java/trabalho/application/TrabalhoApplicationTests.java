3321de7d846f3a9317f7b21b0b8fe1a0bab7026a
Guilhermo Herter <guilhermoh@gmail.com>
2023-05-04 14:40:58 -0300

    adding spring

---
+++ src/test/java/trabalho/application/TrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package trabalho.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
